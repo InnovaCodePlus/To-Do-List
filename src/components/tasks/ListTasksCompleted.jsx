defbb90ce78d27e3be2f2d9e698fbcc46d2cde4a
Mattias <duartemattias3@gmail.com>
2023-05-12 16:37:35 -0400

    refactor and complete project

--- src/components/tasks/ListTasksCompleted.jsx
+++ src/components/tasks/ListTasksCompleted.jsx
@@ -2,15 +2,26 @@ import React from 'react'
 import { CardTask } from '.'
 
 export const ListTasksCompleted = ({ tasks, setTasks }) => {
+
+    const tasksCompleted = tasks.filter((task) => task.status == true )
+
+
     return (
         <>
             <h2 className='text-2xl mb-3'>Tareas completas</h2>
             {
-                tasks?.map(task => {
-                    if( task.status ){
-                        return <CardTask task={task} tasks={tasks}  setTasks={ setTasks } key={ task.id }/>
-                    }
-                })
+                tasksCompleted.length > 0
+                ? (
+                    tasksCompleted?.map(task => 
+                        // if( task.status ){
+                            <CardTask task={task} tasks={tasks}  setTasks={ setTasks } key={ task.id }/>
+                        // }
+                    )
+
+                )
+                : (
+                    <h3>No se completo ninguna tarea aun</h3>
+                )
             }
         </>
     )
--- src/components/tasks/ListTasksPending.jsx
+++ src/components/tasks/ListTasksPending.jsx
@@ -1,15 +1,23 @@
 import { CardTask } from './CardTask'
 
 export const ListTasksPending = ({ tasks, setTasks }) => {
+    
+    const tasksPending = tasks.filter((task) => task.status == false )
+
+
     return (
         <>
             <h2 className='text-2xl mb-3'>Tareas Pendientes</h2>
             {
-                tasks?.map(task => {
-                    if( !task.status ){
-                        return <CardTask task={task} tasks={tasks} setTasks={ setTasks } key={ task.id }/>
-                    }
-                })
+                tasksPending.length > 0
+                ? (
+                    tasksPending?.map(task =>
+                        <CardTask task={task} tasks={tasks} setTasks={ setTasks } key={ task.id }/>
+                    )
+                )
+                : (
+                    <h3>No hay tareas pendientes</h3>
+                )
             }
         </>
     )
